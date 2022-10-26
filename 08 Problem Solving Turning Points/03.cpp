9b569148165711d38355e0312b76cf00cc160951
Talha <talhariaz324@gmail.com>
2022-10-26 22:47:02 +0500

    Cat and mouse problem

--- 08 Problem Solving Turning Points/03.cpp
+++ 08 Problem Solving Turning Points/03.cpp
@@ -1,3 +1,12 @@
 /*
 Turning point was on the logic in if condition. I also have to put = there also. So always check if condition.
 */
+
+/*
+ i use loops for handling counter and then get answer.
+ But instead i can use directly subtraction.
+ z-x gives number of places far from the mouse
+ z-y gives number of places far from the mouse
+ compare and give answer. 
+ But maybe sometimes x or y greater than z then for just use abs(z-x) and abs(z-y). This gives -1 to 1 means positive with same magnitude.
+*/
