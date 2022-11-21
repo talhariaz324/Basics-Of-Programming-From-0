0d23bb3ed03f7cf734bfbe600305fd5ed1435610
Talha <talhariaz324@gmail.com>
2022-11-22 02:27:32 +0500

    a program with debug check

---
+++ 04 DSA Abdul Bari Notes/Recursion/Recursion Implement with Debug/Recursion Implement with Debug/Recursion Implement with Debug.cpp
@@ -0,0 +1,17 @@
+// Recursion Implement with Debug.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+void func1(int n) { // Assume function takes T(n) time
+    if (n > 0) { // 1 
+        cout << n << endl; // 1
+        func1(n - 1); // As we assume that above function takes T(n) time then this function absolutlely takes T(n-1) as per its param
+    }
+}
+
+int main() {
+    int x = 3;
+    func1(x);
+}
+
