b3a02aa7f17fc547a4ff59bea9af1190f1a43c18
Talha <talhariaz324@gmail.com>
2022-11-25 01:41:20 +0500

    Static Variable in recursuion implemented

--- 04 DSA Abdul Bari Notes/Recursion/Recursion Implement with Debug/Recursion Implement with Debug/Recursion Implement with Debug.cpp
+++ 04 DSA Abdul Bari Notes/Recursion/Recursion Implement with Debug/Recursion Implement with Debug/Recursion Implement with Debug.cpp
@@ -1,6 +1,6 @@
 // Recursion Implement with Debug.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
-
+/*
 #include <iostream>
 using namespace std;
 void func1(int n) { // Assume function takes T(n) time
@@ -15,3 +15,31 @@ int main() {
     func1(x);
 }
 
+
+*/
+
+
+
+// Static Varibale in recursion
+
+#include <stdio.h>
+int fun(int n)
+{
+    static int x = 0;
+    if (n > 0)
+    {
+        x++;
+        return fun(n - 1) + x;
+    }
+    return 0;
+}
+int main() {
+    int r;
+    r = fun(5);
+    printf("%d\n", r);
+
+    r = fun(5);
+    printf("%d\n", r);
+
+    return 0;
+}
