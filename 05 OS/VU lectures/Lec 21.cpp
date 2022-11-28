bab874b6a5635cdd5ec153ed1ea36c6fc31ad182
Talha <talhariaz324@gmail.com>
2022-11-29 01:01:06 +0500

    lec 21 understanding

--- 05 OS/VU lectures/Lec 21.cpp
+++ 05 OS/VU lectures/Lec 21.cpp
@@ -2,7 +2,7 @@
 Sync Hardware:
 
 Normally, access to a memory location excludes other accesses to that
-same location.
+same location. Means one process can access a memory location ,,, other processes can't at that time.
 
 Extension: Designer have proposed machine instructions that perform 2 operations
 atomically (indivisibly) on the same memory location.
@@ -10,7 +10,7 @@ For Example: Read and write.
 
 Note: Atomically means that one process doing both things. by using true and false,
 
-The execution if such an instruction is also mutally exclusive (even on microprocessor)
+The execution if such an instruction is also mutually exclusive (even on microprocessor)
 
 They can be used to provide mutual exclusion but other mechanisms are needed to satisfy the other 2 requirements of a good 
 solution to the CS problem.
@@ -27,9 +27,9 @@ boolean TestAndSet (boolean &target){
     return rv;
 }
 
--> Lock is set to false
+-> Lock is set to false and its a global variable and access by the other processes as well.
 do{
-    while(TestAndSet(lock)){
+    while(TestAndSet(lock)){ // Only go inside when lock is false. if some process is in cs then it make the lock to true so the while condition will remain true 
         CS
         lock  = true;
     }
@@ -54,9 +54,9 @@ void swap (boolean &a, boolean &b){
 }
 key is local and set to false.
 do{
-    key = true;
-    while(key == true ){
-        swap(lock, key);
+    key = true; // 1st process come and its key true
+    while(key == true ){ // if key is true
+        swap(lock, key); // make swap,,, now swap make the lock true and the other processes not allowed to enter CS
         CS
     }
     lock = false;
