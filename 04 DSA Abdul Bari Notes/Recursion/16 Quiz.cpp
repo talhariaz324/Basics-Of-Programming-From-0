4b1b4ab633f48c8aad3237bbdc6dee0f282ac6a2
Talha <talhariaz324@gmail.com>
2022-12-15 01:20:20 +0500

    quiz done

---
+++ 04 DSA Abdul Bari Notes/Recursion/16 Quiz.cpp
@@ -0,0 +1,10 @@
+/*
+Quiz:
+All done by me with little mistakes.
+1- Call by value means new copy so it will not have impact on passing one.
+2- Large number of calls cannot be trace using tracing so we make a table
+table includes from smaller value.
+for example: search for f(5);
+Then instead directly search for 5 we have to search 1 then 2 and.... 5
+See Quiz at 25 mint
+*/
