f7abb4c6fdab178694f4baa7b2fe49860ef4b098
Talha <talhariaz324@gmail.com>
2022-12-19 01:44:14 +0500

    Array

---
+++ 04 DSA Abdul Bari Notes/Arrays Representation/01. intro to array.cpp
@@ -0,0 +1,17 @@
+/*
+You can go for Sir Fareed for more on the arrays.
+
+Simple variables are the scalars
+Array is the vector
+
+Baisc array get space in stack.
+It size can be given compile time and runtime as well.
+But once the size given, it cannot be extended.
+Simple
+A[5] = {1,2,3,4};
+
+But in heap creation we use new keyword.
+This will make the array on heap which can grow.
+We can access it using pointer.
+*/
+
